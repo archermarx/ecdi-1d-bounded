946d68be401ccc6d1b2f093ff563b36ca10381bc
Thomas Marks <marksta@umich.edu>
2023-12-08 14:00:51 -0500

    compute heat flux (it's negative?)

--- heat_flux.jl
+++ heat_flux.jl
@@ -1,4 +1,19 @@
 using StatsBase
+using NumericalIntegration
+
+function heat_flux(vel, mass)
+    # compute square of speed for all particles
+    v² = mapslices(x -> sum(x.^2), vel, dims = 2)[:, 1]
+
+    # get axial velocity
+    vx = vel[:, 1]
+
+    # compute heat flux in eV / m^2 s
+    integrand = @. 0.5 * mass * vx * v² / q_e
+    q = mean(integrand)
+
+    return q
+end
 
 function select_species(data, species_name)
     if (species_name in ["electrons", "e", "electron"])
@@ -10,7 +25,7 @@ function select_species(data, species_name)
     end
 end
 
-function temperature_gradient(data, species, iter, index; bin_factor = 1, center_fraction = 0.5, offset_fraction = 0.15)
+function temperature_gradient(data, species, iter, index; plot = true, bin_factor = 1, center_fraction = 0.5, offset_fraction = 0.15)
     species_data = select_species(data, species)
 
     x = species_data.position[:, 1, iter]
@@ -30,12 +45,14 @@ function temperature_gradient(data, species, iter, index; bin_factor = 1, center
     bin_id = 1
     last = 1
     temperatures = zeros(num_bins)
+    bin_assignments = zeros(Int, num_particles)
     for (i, _x) in enumerate(x_sorted)
         if _x ≥ edges[bin_id+1] || i == length(x_sorted)
-            temperatures[bin_id] = mass * var(v_sorted[last:i]) / q_e
+            temperatures[bin_id] = mass * var(v_sorted[last:i-1]) / q_e
             bin_id += 1
-            last = i + 1
+            last = i
         end
+        bin_assignments[i] = bin_id
     end
 
     CairoMakie.activate!()
@@ -50,34 +67,44 @@ function temperature_gradient(data, species, iter, index; bin_factor = 1, center
 
     xc = center_positions[center_inds]
     m, b, std, std_m, std_b = fit_line(xc, temperatures[center_inds])
-    #=
-    T_pred = @. m * xc + b
-    lower = @. T_pred - 3*std
-    upper = @. T_pred + 3*std
 
+    #@show [center_inds]
+    vel_inds = findall(i -> i ∈ center_inds, bin_assignments)
+    #@show vel_inds
+    vel_center = species_data.velocity[perm[vel_inds], :, iter]
 
-    fig, ax, b = band(xc, lower, upper; color = (:grey, 0.2))
-    plot!(ax, center_positions, temperatures)
-    plot!(ax, center_positions[center_inds], temperatures[center_inds])
-    lines!(ax, xc, T_pred; linestyle = :dash, linewidth = 2, color = :black)
-    display(fig)
-    =#
-    return m, std_m
+    q = heat_flux(vel_center, mass)
+
+    if (plot)
+        T_pred = @. m * xc + b
+        lower = @. T_pred - 3*std
+        upper = @. T_pred + 3*std
+
+        fig, ax, b = band(xc, lower, upper; color = (:grey, 0.2))
+        plot!(ax, center_positions, temperatures)
+        plot!(ax, center_positions[center_inds], temperatures[center_inds])
+        lines!(ax, xc, T_pred; linestyle = :dash, linewidth = 2, color = :black)
+        display(fig)
+    end
+    return m, std_m, q
 end
 
 function all_temp_gradients(data, species, index; kwargs...)
     N = length(data.iter)
 
-    results = [temperature_gradient(data, species, i, index; kwargs...) for i in 1:N]
+    results = [temperature_gradient(data, species, i, index; plot = false, kwargs...) for i in 1:N]
     ∇T = [res[1] for res in results] ./ 1000
     σ = [res[2] for res in results] ./ 1000
+    q = [res[3] for res in results] ./ 1e6
     lower = @. ∇T - 3 * σ
     upper = @. ∇T + 2 * σ
 
+    colors = Makie.wong_colors()
+
     t_μs = data.time * 1e6
-    fig, ax, b = band(t_μs, lower, upper; color = (:grey, 0.2))
-    lines!(ax, t_μs, ∇T)
-    ylims!(ax, -20, 1)
+    fig, ax, b = band(t_μs, lower, upper; color = (colors[1], 0.2))
+    lines!(ax, t_μs, ∇T; color = colors[1])
+    lines!(ax, t_μs, q; color = colors[2])
     ax.xlabel = "Time (μs)"
     ax.ylabel = "Electron temperature gradient (eV / mm)"
 
